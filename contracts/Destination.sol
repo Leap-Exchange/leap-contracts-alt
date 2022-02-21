977fe9f58a320d17bb0c0611827e282dbe2292ed
shr1ftyy <syeamtechdemon@gmail.com>
2022-02-21 16:58:04 +1030

    it's grind time boii

--- contracts/Destination.sol
+++ contracts/Destination.sol
@@ -1,32 +1,32 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity 0.8.11;
 pragma experimental ABIEncoderV2;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import './Data.sol';
+import './utils/Data.sol';
 
-// [TODO]
-// import openzeppelin ERC20 Interface so we can use ERC20 token functions
-
-// temporary stand in for IBC contract
+// temporary stand-in for an IBC contract
 interface ICrossChainMessenger {
   function sendMsg(address contract_addr, bytes memory message, uint256 gas) external;
 
 }
 
 contract Destination {
-  address public owner = msg.sender;
-  // transferOwners: (transferData, transferID) => address owner
-  // might change state map implementation in the future
-  // mapping(struct => mapping(uint => address)) public transferOwners; -> This does not work,
-  // since maps cannot utilise complex objects such as structs as keys, instead,
-  // the the arguments in TransferData will be concatenated and turned into bytes 
-  // before indexing transferOwners.
-  mapping(bytes => mapping(uint => address)) public transferOwners;
-  mapping(bytes32 => bool) public claimedTransferHashes;
+  address public owner;
+  address public L2SourceAddr;
+  mapping(bytes32 => bool) claimedTransferHashes;
   uint256 public transferCount; 
   bytes32[] rewardHashOnionHistoryList;
+  uint256 public lastSourceHeadPosition;
 
+  constructor(address L2SourceAddress) {
+    L2SourceAddr = L2SourceAddress;
+    owner = msg.sender;
+    transferCount = 0;
+    rewardHashOnionHistoryList.push(keccak256(abi.encodePacked('0')));
+  }
+
+  event TransferClaimed(bytes32 transferHash, uint256 transferNum);
 
   modifier restricted() {
     require(
@@ -36,21 +36,38 @@ contract Destination {
     _;
   }
 
-  function claim(Data.TransferData memory transferData) public {
+  // Attempting to claim a transfer that does not exist on the source side will not benefit the claimer with any rewards. 
+  // Call with caution :p
+  function claim(Data.TransferData memory transferData) public payable{
     // improve hashing implementation - optimize for least gas usage. For now, the following will have to do
-    bytes32 transferHash = keccak256(abi.encode(transferData));
-    require(!claimedTransferHashes[transferHash]);
-    IERC20(transferData.tokenAddress).transferFrom(msg.sender, address(this), transferData.amount);
+    bytes32 transferHash = keccak256(abi.encodePacked(abi.encode(transferData), transferCount));
+    require(!claimedTransferHashes[transferHash], 'this transfer has already been claimed OR does not exist');
+    
+    if (transferData.destTokenAddress != address(0))
+    {
+      IERC20(transferData.destTokenAddress).transferFrom(msg.sender, transferData.destination, transferData.amount);
+      // IERC20(transferData.destTokenAddress).transferFrom(msg.sender, address(this), transferData.amount);
+    } 
+   else{
+      // Transfer ether here
+      require(msg.value >= 0.001 ether, 'minimum ether required is 0.001 ether');
+      require(msg.value == transferData.amount, 'insufficient ether provided');
+      payable(transferData.destination).transfer(transferData.amount);
+    }
+
     claimedTransferHashes[transferHash] = true;
     uint256 currentTime = block.timestamp;
-    Data.RewardData memory rewardData = Data.RewardData(transferHash, transferData.tokenAddress, transferData.destination, getLPFee(transferData, currentTime));
+    Data.RewardData memory rewardData = Data.RewardData(transferHash, transferData.destTokenAddress, transferData.destination, getLPFee(transferData, currentTime));
     bytes32 rewardHash = keccak256(abi.encode(rewardData));
     bytes32 rewardHashOnion = keccak256(abi.encode(rewardHash, rewardData));
     transferCount++;
+    emit TransferClaimed(transferHash, transferCount);
+    // Will need to address this update rate later
+    // if (transferCount % 100 == 0){
+    //   rewardHashOnionHistoryList.push(rewardHashOnion);
+    // }
 
-    if (transferCount % 100 == 0){
-      rewardHashOnionHistoryList.push(rewardHashOnion);
-    }
+    rewardHashOnionHistoryList.push(rewardHashOnion);
 
   }
 
@@ -63,20 +80,41 @@ contract Destination {
     }
     else{
       // Note: this clause is unreachable if feeRampup == 0
-      // [TODO] Implement int divide '//' operator in solidity for return statement below
-      return uint256(transfer.fee * (currentTime - transfer.startTime)) / transfer.feeRampup; 
+      return transfer.fee * (currentTime - transfer.startTime) / transfer.feeRampup; 
     }
     
   }
 
-  function declareNewHashChainHead(address L1SourceAddr, bytes memory message, uint256 gas) public {
-    ICrossChainMessenger(L1SourceAddr).sendMsg(L1SourceAddr, message, gas);
+  function declareNewHashChainHead() public {
+    require(rewardHashOnionHistoryList.length < lastSourceHeadPosition, 'no new hash onion');
+    bytes32[] memory rewardList = new bytes32[](rewardHashOnionHistoryList.length - lastSourceHeadPosition);
+    for(uint i = lastSourceHeadPosition; i <= rewardHashOnionHistoryList.length; i++){
+      rewardList[i - lastSourceHeadPosition] = rewardList[i];
+    }
+    //temp vars
+    bytes memory message = bytes('0');
+    uint gas = 0;
+    ICrossChainMessenger(L2SourceAddr).sendMsg(L2SourceAddr, message, gas);
   }
 
-  function changeOwner(Data.TransferData memory transferData, uint transferID, address newOwner) public { }
+  // only the owner of the contract can set he new address of the source contract - for now...
+  function changeL2SourceAddr(address newAddress) public restricted{ 
+    L2SourceAddr = newAddress;
 
-  function buy(Data.TransferData memory transferData, uint transferID) public {}
+  }
 
+  // fallback function
+  fallback() external{
+  }
+
+  // temporary - will probably not be included in the final implementation
+  function changeOwner(address newOwner) public restricted { 
+    require(owner != address(0), '0x0 address cannot be set as the owner!');
+    require(owner != newOwner, 'this address is already the owner of the contract');
+    owner = newOwner;
+  }
+
+  // function buy(Data.TransferData memory transferData, uint transferID) public {}
 
 
 }
--- contracts/utils/Data.sol
+++ contracts/utils/Data.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity 0.8.11;
 
 library Data{
   struct TransferData { 
-    address tokenAddress;
+    address srcTokenAddress;
+    address destTokenAddress;
     address destination;
     uint256 amount;
     uint256 fee;
     uint256 startTime;
     uint256 feeRampup;
-    uint256 nonce;
   }
 
   struct RewardData { 
 		bytes32 transferDataHash;
-    address tokenAddress;
+    address srcTokenAddress;
     address claimer;
     uint256 fee;
   }
